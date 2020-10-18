5b81a3f83d001c7a2e7f9c1ade639bfe65715610
zubi <azu@gmail.com>
2020-10-18 19:40:13 +0100

    nade some adjust

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.port || '4040');
+  await app.listen(process.env.port || 4040);
   Logger.log(console.log(`server running on port 4040`));
 }
 bootstrap();
