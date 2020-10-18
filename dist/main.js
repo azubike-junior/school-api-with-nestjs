90ee387227433cc0e6d8341d59450800c46d6fdd
zubi <azu@gmail.com>
2020-10-18 17:23:00 +0100

    nade some adjust

--- dist/main.js
+++ dist/main.js
@@ -4,11 +4,10 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 require("dotenv/config");
 const common_1 = require("@nestjs/common");
-const port = process.env.port || 4040;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(port);
-    common_1.Logger.log(console.log(`server running on port ${port}`));
+    await app.listen(process.env.port || '4040');
+    common_1.Logger.log(console.log(`server running on port 4040`));
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { Logger } from '@nestjs/common';
-import { createConnection } from 'net';
 
-const port = process.env.port || 4040;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
-  Logger.log(console.log(`server running on port ${port}`));
+  await app.listen(process.env.port || '4040');
+  Logger.log(console.log(`server running on port 4040`));
 }
 bootstrap();
