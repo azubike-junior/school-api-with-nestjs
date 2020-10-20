6a7a72e3873d3f068c0ab501c439c5d5da7b867e
zubi <azu@gmail.com>
2020-10-20 11:37:56 +0100

    changed main.ts

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,23 +1,23 @@
-import {ConnectionOptions} from 'typeorm';
-import 'dotenv/config';
-const { DB_HOST, DB_PORT, DEV_DB, DB_PASSWORD, DATABASE_URL, DB_NAME, NODE_ENV } = process.env;
+// import {ConnectionOptions} from 'typeorm';
+// import 'dotenv/config';
+// const { DB_HOST, DB_PORT, DEV_DB, DB_PASSWORD, DATABASE_URL, DB_NAME, NODE_ENV } = process.env;
 
-const env = NODE_ENV === 'development' ? DEV_DB : DATABASE_URL
+// const env = NODE_ENV === 'development' ? DEV_DB : DATABASE_URL
 
-const config: ConnectionOptions = {
-    type: 'postgres',
-      url: env,
-      synchronize: false,
-      migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-      migrationsRun: true,
-      dropSchema: false,
-      logging: false,
-      entities: ['dist/**/*{.ts,.js}'],
-      cli: {
-        entitiesDir: 'src/entity',
-        migrationsDir: 'src/migrations',
-        subscribersDir: 'src/subscriber',
-    },
-};
+// const config: ConnectionOptions = {
+//     type: 'postgres',
+//       url: env,
+//       synchronize: false,
+//       migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+//       migrationsRun: true,
+//       dropSchema: false,
+//       logging: false,
+//       entities: ['dist/**/*{.ts,.js}'],
+//       cli: {
+//         entitiesDir: 'src/entity',
+//         migrationsDir: 'src/migrations',
+//         subscribersDir: 'src/subscriber',
+//     },
+// };
 
-export = config;
+// export = config;
